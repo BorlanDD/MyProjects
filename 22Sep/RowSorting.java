0e029df3ee8fa8e676616e0288fd69ddf4910d4e
BorlanDD <fatyhov.1996@mail.ru>
2016-09-28 22:24:34 +0300

    Update and rename SortRows.java to RowSorting.java

--- 22Sep/RowSorting.java
+++ 22Sep/RowSorting.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class SortRows {
+class RowSorting {
   public static void main (String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter the amount of rows in the array: ");
